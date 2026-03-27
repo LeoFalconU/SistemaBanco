0509e72e55ad501eb51179d8bdf1d9813bfe525c
LeoFalconU <20253rd053@utez.edu.mx>
2026-03-27 15:56:57 -0600

    "Se agrego el sistema de validacion de credenciales para el inicio de sesion"

--- src/main/java/com/example/sistemabanco/HelloController.java
+++ src/main/java/com/example/sistemabanco/HelloController.java
@@ -1,14 +1,44 @@
 package com.example.sistemabanco;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField usuario;
+    @FXML
+    private TextField cont;
+    @FXML
+    private Label errorr;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void validarAcceso(ActionEvent event) {
+        String user = usuario.getText();
+        String passwd = cont.getText();
+
+        if (user.equals("admin") && passwd.equals("admin123") ) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Menu.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = new Stage();
+                stage.setTitle("Segunda Ventana");
+                stage.setScene(new Scene(root));
+                stage.show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {errorr.setVisible(true);}
     }
+
+
 }
+
