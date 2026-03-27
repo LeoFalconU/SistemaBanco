6e30901fbb131e1bcedb940b2efc3296195142bd
LeoFalconU <20253rd053@utez.edu.mx>
2026-03-27 16:50:53 -0600

    "Se preparo el scene de consulta de saldo y la vinculacion con ventana del controlloer"

---
+++ src/main/java/com/example/sistemabanco/ConsultarController.java
@@ -0,0 +1,18 @@
+package com.example.sistemabanco;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ConsultarController {
+
+
+    @FXML
+    private Label usuarioLabel;
+    @FXML
+    private Label saldoLabel;
+
+    @FXML
+    public void initialize() {
+        usuarioLabel.setText();
+    }
+}
